b176b6296c728991875d602abcbfb3787a208acb
yume15 <g12915iy@gm.tsuda.ac.jp>
2014-05-01 12:44:34 +0900

    テスト作成中

---
+++ snake-camel/test/snakecamel/SnakeCamelTest.java
@@ -0,0 +1,16 @@
+package snakecamel;
+
+import static org.junit.Assert.*;
+import junit.tutorial.Calculator;
+
+import org.junit.Test;
+
+public class SnakeCamelTest {
+
+	@Test
+	public void test() {
+		SnakeCamelUtil snake = new SnakeCamelUtil();
+		
+	}
+
+}
